9af0d8038d575e03e430e49817a2f19cd8ab9da6
Munna1680 <shakibhassanmunna777@gmail.com>
2026-09-25 10:54:45 +0600

    numbers done

---
+++ Python/3_numbers.py
@@ -0,0 +1,18 @@
+# x = 1
+# y = 2.8
+# z = 4 + 1j
+# print(x , y , z)
+
+# y = 2435087309805749437
+# print(y)
+
+# y = 35e3
+# print(y)
+
+# x = complex(5+8j)
+# print(x)
+
+# import random 
+
+# print(random.randrange(1000,10000))
+
