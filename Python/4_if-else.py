fee28b53654d60098a4e90ba26a4df516e68b4f3
Munna1680 <shakibhassanmunna777@gmail.com>
2026-09-25 11:17:35 +0600

    if else done

---
+++ Python/4_if-else.py
@@ -0,0 +1,28 @@
+# a = 33
+# b = 200
+
+# if b<a:
+#     print("b is smaller than a") 
+# elif a == b:
+#     print("a is equal to b")
+# else:
+#     print("a is less than b")
+
+a = 300
+b = 200
+c = 100
+
+if a>b and b>c:
+    print("the 1st logic is true")
+
+if a>b or b>c:
+    print("the 2nd logic is true")
+
+if not a<b:
+    print("true")
+    if a>20:
+        print("nested if success")
+else:
+    print("failed")
+
+# ami jodi if er condition er jaygay pass lekhi tahole if ta true hole kichui print hobe na
